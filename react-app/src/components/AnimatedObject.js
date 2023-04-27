7dff566a0cf423f96b1f890eed63228d4c8e3b55
Ernesto Perez La Rosa <eperezlarosa@cisco.com>
2023-04-27 19:49:09 -0400

    Working Square animation

--- react-app/src/components/AnimatedObject.js
+++ react-app/src/components/AnimatedObject.js
@@ -2,25 +2,20 @@ import React from 'react';
 import '../styles/AnimatedObject.css';
 
 function AnimatedObject() {
-    const squares = [
-        { message: 'Service' },
-        { message: 'Message1' },
-        { message: 'Message2' },
-        { message: 'Message3' },
-        { message: 'Message4' },
-    ];
+    const numSquares = 5;
+    const squares = [];
+
+    for (let i = 0; i < numSquares; i++) {
+        squares.push(
+            <div key={i} className="animated-object" style={{ animationDelay: `${i * 0.2}s`, left: `${i * 50}px`, top: `${i * 50}px` }}>
+                {i === 0 ? 'Service' : `Message${i}`}
+            </div>
+        );
+    }
 
     return (
-        <div>
-            {squares.map((square, index) => (
-                <div
-                    key={index}
-                    className="animated-object"
-                    style={{ animationDelay: `${index * 0.2}s` }}
-                >
-                    {square.message}
-                </div>
-            ))}
+        <div className="animated-object-wrapper">
+            {squares}
         </div>
     );
 }
