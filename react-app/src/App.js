95e5e0ca2fb7ec08d41e7889201a4103148258e5
Ernesto Perez La Rosa <eperezlarosa@cisco.com>
2023-04-27 09:34:10 -0400

    beautiful front-end

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap';
+import { Nav, Navbar, Container, NavDropdown, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/navbar.css';
 
@@ -11,17 +11,20 @@ function ProgrammerQ() {
                     <Navbar.Brand href="#home">ProgrammerQ</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mx-auto">
+                        <Nav className="flex-grow-1">
                             <Nav.Link href="#home">Home</Nav.Link>
                             <Nav.Link href="#projects">Projects</Nav.Link>
                             <Nav.Link href="#blog">Blog</Nav.Link>
                             <Nav.Link href="#about">About</Nav.Link>
-                            <NavDropdown title="More" id="basic-nav-dropdown">
+                            <NavDropdown title="More" id="basic-nav-dropdown" className="dropdown-more">
                                 <NavDropdown.Item href="#action/3.1">Contact</NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.2">Privacy Policy</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Terms and Conditions</NavDropdown.Item>
                             </NavDropdown>
+                        </Nav>
+                        <Nav className="ml-auto">
+                            <Button variant="outline-warning">Login</Button>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
