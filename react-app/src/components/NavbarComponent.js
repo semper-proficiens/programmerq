d68694ede114082651c6d2c396c8217d8ed602c5
Ernesto Perez La Rosa <eperezlarosa@cisco.com>
2023-04-27 23:19:29 -0400

    Components more aligned

--- react-app/src/components/NavbarComponent.js
+++ react-app/src/components/NavbarComponent.js
@@ -9,20 +9,20 @@ function NavbarComponent() {
             <Container>
                 <Navbar.Brand href="#home">ProgrammerQ</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
-                    <Nav className="flex-grow-1 justify-content-center">
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mx-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#projects">Projects</Nav.Link>
                         <Nav.Link href="#blog">Blog</Nav.Link>
                         <Nav.Link href="#about">About</Nav.Link>
+                    </Nav>
+                    <Nav className="ml-auto">
                         <NavDropdown title="More" id="basic-nav-dropdown" className="dropdown-more">
                             <NavDropdown.Item href="#action/3.1">Contact</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.2">Privacy Policy</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Terms and Conditions</NavDropdown.Item>
                         </NavDropdown>
-                    </Nav>
-                    <Nav className="ml-auto">
                         <Button variant="outline-warning">Login</Button>
                     </Nav>
                 </Navbar.Collapse>
