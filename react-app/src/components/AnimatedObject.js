8abd508e1916658337ce34bcc059ee38a2438560
Ernesto Perez La Rosa <eperezlarosa@cisco.com>
2023-04-27 20:13:13 -0400

    Perfect size and animation squares queue

--- react-app/src/components/AnimatedObject.js
+++ react-app/src/components/AnimatedObject.js
@@ -7,7 +7,7 @@ function AnimatedObject() {
 
     for (let i = 0; i < numSquares; i++) {
         squares.push(
-            <div key={i} className="animated-object" style={{ animationDelay: `${i * 0.2}s`, top: `${(i + 1) * 50}px` }}>
+            <div key={i} className="animated-object" style={{ animationDelay: `${i * 0.2}s`, top: `${(i + 1) * 100}px` }}>
                 {i === 0 ? 'Programmer...' : `Queue${i}`}
             </div>
         );
