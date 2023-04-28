b3d48caa2bfb19cd6209fa8d7c41f3ebaa523713
Ernesto Perez La Rosa <eperezlarosa@cisco.com>
2023-04-28 00:09:35 -0400

    Adding effect to the welcome message

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import Typing from 'react-typing-animation';
 
 function Home() {
+    const fontStyles = {
+        fontFamily: 'Courier New',
+        fontWeight: 'bold',
+        fontSize: '32px',
+        letterSpacing: '1px',
+        lineHeight: '48px',
+    };
+
     return (
         <Container>
-            <h1>Welcome to ProgrammerQ!</h1>
+            <Typing speed={300}>
+                <h1 style={fontStyles}>
+                    {Array.from("Welcome to ProgrammerQ!").map((letter, index) => (
+                        <span
+                            key={index}
+                            style={{
+                                color: `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+                            }}>
+              {letter}
+            </span>
+                    ))}
+                </h1>
+            </Typing>
             <p>Here's some content for the home page.</p>
         </Container>
     );
