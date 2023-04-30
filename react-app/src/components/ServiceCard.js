fd7bcbfd590929ac3f7c0a10e0f5b0a554dff6d8
Ernesto Perez La Rosa <eperezlarosa@cisco.com>
2023-04-30 12:45:01 -0400

    Adding style to the card

--- react-app/src/components/ServiceCard.js
+++ react-app/src/components/ServiceCard.js
@@ -3,14 +3,14 @@ import { Card, Button } from 'react-bootstrap';
 
 function ServiceCard({ service }) {
     return (
-        <Card>
+        <Card style={{ border: '2px solid #28282B' }}>
             <Card.Img variant="top" src={service.imageUrl} />
             <Card.Body>
                 <Card.Title>{service.title}</Card.Title>
                 <Card.Text>
                     {service.description}
                 </Card.Text>
-                <Button href={service.link} variant="primary">Learn More</Button>
+                <Button href={service.link} variant="outline-dark">Learn More</Button>
             </Card.Body>
         </Card>
     );
