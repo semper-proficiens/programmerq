a3023b6743244521884c30d2026a69fd3c22e692
Ernesto Perez La Rosa <eperezlarosa@cisco.com>
2023-04-30 13:44:28 -0400

    Beautiful Blog section

--- react-app/src/components/Blog.js
+++ react-app/src/components/Blog.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import '../styles/blog.css';
 
 function Blog() {
     return (
-        <Container id="blog">
+        <Container id="blog" className="my-5">
             <h2>Blog</h2>
-            <p>Here's some content for the blog page.</p>
+            <div className="blog-box">
+                <p className="typewriter">
+                    Here's some content for the <a href="/blog" className="text-decoration-none">blog page</a>.
+                </p>
+            </div>
         </Container>
     );
 }
