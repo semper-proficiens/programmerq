89d764e92443fdaff1624358df3af1b19a370739
Ernesto Perez La Rosa <eperezlarosa@cisco.com>
2023-05-01 01:12:20 -0400

    Make DarkMode the default

--- react-app/src/contexts/DarkModeContext.js
+++ react-app/src/contexts/DarkModeContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react';
 export const DarkModeContext = createContext(null);
 
 export function DarkModeProvider(props) {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(true);
 
     return (
         <DarkModeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
