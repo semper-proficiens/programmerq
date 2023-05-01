c60a4bc93a65ca76850bf24fe033c71f445ec88f
Ernesto Perez La Rosa <eperezlarosa@cisco.com>
2023-05-01 01:04:10 -0400

    Pass dark mode to WIP pages

--- react-app/src/components/WorkInProgress.js
+++ react-app/src/components/WorkInProgress.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Container } from 'react-bootstrap';
 import dashboardImage from '../assets/images/build.gif';
+import { DarkModeContext } from '../contexts/DarkModeContext';
 
 function WorkInProgress() {
+    const { isDarkMode } = useContext(DarkModeContext);
+
     return (
         <Container id="blog" className="my-5">
             <section>
-                <h1 className="display-5 text-center m-4 "><span role="img" aria-label="sorry">😖</span> Sorry, this page is under construction</h1>
+                <h1 className={isDarkMode ? "display-5 text-center m-4 text-white" : "display-5 text-center m-4 text-black"}><span role="img" aria-label="sorry">😖</span> Sorry, this page is under construction</h1>
             </section>
             <div className="media">
                 <img className="mr-3 align-self-center m-auto" src={dashboardImage} alt="work in progress"/>
