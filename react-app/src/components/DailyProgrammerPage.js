1384c0c30ab8f193e7e62746476d5a00df780824
Ernesto Perez La Rosa <eperezlarosa@cisco.com>
2023-05-01 18:18:58 -0400

    Initial attempt at passing items inside the widget

--- react-app/src/components/DailyProgrammerPage.js
+++ react-app/src/components/DailyProgrammerPage.js
@@ -3,15 +3,23 @@ import '../styles/widgets.css';
 
 function DailyProgrammerPage() {
     const [widgets, setWidgets] = useState([{},{},{}]);
+    const [showOptions, setShowOptions] = useState(false);
+    const [selectedOption, setSelectedOption] = useState(null);
 
     const handleAddWidget = () => {
-        setWidgets([...widgets, {}]);
+        setShowOptions(true);
     };
 
     const handleRemoveWidget = (index) => {
         const newWidgets = [...widgets];
         newWidgets.splice(index, 1);
         setWidgets(newWidgets);
+    };
+
+    const handleSelectOption = (option) => {
+        setSelectedOption(option);
+        setWidgets([...widgets, option]);
+        setShowOptions(false);
     };
 
     return (
@@ -23,7 +31,9 @@ function DailyProgrammerPage() {
                             <button onClick={() => handleRemoveWidget(index)}>{index === widgets.length - 1 ? '-' : '-'}</button>
                         </div>
                         <div className="widget-content">
-                            {/* Content to be supplied from backend */}
+                            {index === widgets.length - 1 && selectedOption && (
+                                <div>{selectedOption.content}</div>
+                            )}
                         </div>
                     </div>
                 ))}
@@ -32,9 +42,29 @@ function DailyProgrammerPage() {
                         <span>+</span>
                     </div>
                 )}
+                {showOptions && (
+                    <div className="options-container">
+                        <div className="options-header">
+                            <button onClick={() => setShowOptions(false)}>X</button>
+                        </div>
+                        <div className="options-list">
+                            {dummyOptions.map((option, index) => (
+                                <div key={index} className="option" onClick={() => handleSelectOption(option)}>
+                                    {option.title}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
 };
 
 export default DailyProgrammerPage;
+
+const dummyOptions = [
+    { title: 'Option 1', content: 'Content for option 1' },
+    { title: 'Option 2', content: 'Content for option 2' },
+    { title: 'Option 3', content: 'Content for option 3' },
+];
