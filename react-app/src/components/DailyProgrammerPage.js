800810699dec7ba04e6c52704104329938833302
Ernesto Perez La Rosa <eperezlarosa@cisco.com>
2023-05-01 11:15:38 -0400

    Adding dropdown items with dark dividers

---
+++ react-app/src/components/DailyProgrammerPage.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+function DailyProgrammerPage() {
+    const [widgets, setWidgets] = useState([]);
+
+    const handleAddWidget = () => {
+        setWidgets([...widgets, {}]);
+    };
+
+    const handleRemoveWidget = (index) => {
+        setWidgets(widgets.filter((_, i) => i !== index));
+    };
+
+    return (
+        <div className="daily-programmer-page">
+            <div className="widgets-container">
+                {widgets.map((widget, index) => (
+                    <div key={index} className="widget">
+                        <div className="widget-header">
+                            <button onClick={() => handleRemoveWidget(index)}>+</button>
+                        </div>
+                        <div className="widget-content">
+                            {/* Content to be supplied from backend */}
+                        </div>
+                    </div>
+                ))}
+                {widgets.length < 9 && (
+                    <div className="add-widget" onClick={handleAddWidget}>
+                        <span>+</span>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default DailyProgrammerPage;
--- react-app/src/components/NavbarComponent.js
+++ react-app/src/components/NavbarComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Nav, Navbar, Container, NavDropdown, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/navbar.css';
@@ -24,18 +24,36 @@ function NavbarComponent() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mx-auto">
                         <Nav.Link as={Link} to="/"><span className="text-primary">H</span>ome</Nav.Link>
-                        <Nav.Link as={Link} to="/services"><span className="text-primary">S</span>ervices</Nav.Link>
+                        <NavDropdown
+                            title={
+                                <span>
+                                    <span className="text-primary">S</span>ervices
+                                </span>
+                            }
+                            id="basic-nav-dropdown"
+                            className="nav-link-dropdown"
+                        >
+                            <NavDropdown.Item href="#action/3.1" className="text-light bg-dark"><span>DailyProgrammer</span><div className="dropdown-divider dark"></div></NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.1" className="text-light bg-dark"><span>Service2</span></NavDropdown.Item>
+                        </NavDropdown>
                         <Nav.Link as={Link} to="/blog"><span className="text-primary">B</span>log</Nav.Link>
                         <Nav.Link as={Link} to="/about"><span className="text-primary">A</span>bout</Nav.Link>
                     </Nav>
                     <Nav className="ml-auto">
-                        <NavDropdown title="More" id="basic-nav-dropdown" className="dropdown-more">
-                            <NavDropdown.Item href="#action/3.1">Contact</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.2">Privacy Policy</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Terms and Conditions</NavDropdown.Item>
+                        <NavDropdown
+                            title={
+                                <span>
+                                    <span className="text-primary">M</span>ore
+                                </span>
+                            }
+                            id="basic-nav-dropdown"
+                            className="nav-link-dropdown"
+                        >
+                            <NavDropdown.Item href="#action/3.1" className="text-light bg-dark"><span>Contact</span><div className="dropdown-divider dark"></div></NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.1" className="text-light bg-dark"><span>Privacy Policy</span><div className="dropdown-divider dark"></div></NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.1" className="text-light bg-dark"><span>Terms and Conditions</span></NavDropdown.Item>
                         </NavDropdown>
-                        <Button as={Link} to="/login" variant="outline-warning">Login</Button>
+                        <Button as={Link} to="/login" variant="outline-warning" className="btn-login">Login</Button>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
