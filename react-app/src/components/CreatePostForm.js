517b2e669b37534e97004bb171c3ddabe1b61b68
Ernie_py <user@example.com>
2023-12-07 00:53:31 -0500

    Adding empty field validation for create post

--- react-app/src/components/CreatePostForm.js
+++ react-app/src/components/CreatePostForm.js
@@ -1,13 +1,34 @@
-import React, { useContext } from 'react';
+// components/CreatePostForm.js
+import React, { useContext, useState } from 'react';
 import { Form, Button, Dropdown } from 'react-bootstrap';
 import { DarkModeContext } from '../contexts/DarkModeContext';
 import '../styles/createpostform.css';
 
 const CreatePostForm = ({ newPost, handleInputChange, handlePostSubmit }) => {
     const { isDarkMode } = useContext(DarkModeContext);
+    const [validation, setValidation] = useState({ title: true, content: true, category: true });
+
+    const isFormValid = () => {
+        const isValid = newPost.title.trim() !== '' && newPost.content.trim() !== '' && newPost.category !== '';
+        setValidation({
+            title: newPost.title.trim() !== '',
+            content: newPost.content.trim() !== '',
+            category: newPost.category !== '',
+        });
+        return isValid;
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (isFormValid()) {
+            handlePostSubmit(e);
+        } else {
+            console.error('Validation failed. Please fill in all fields and select a category.');
+        }
+    };
 
     return (
-        <Form onSubmit={handlePostSubmit}>
+        <Form onSubmit={handleSubmit}>
             <Form.Group controlId="postTitle">
                 <Form.Label>Title</Form.Label>
                 <Form.Control
@@ -17,6 +38,7 @@ const CreatePostForm = ({ newPost, handleInputChange, handlePostSubmit }) => {
                     value={newPost.title}
                     onChange={handleInputChange}
                 />
+                {!validation.title && <div className="validation-message">Title can't be empty</div>}
             </Form.Group>
             <Form.Group controlId="postContent">
                 <Form.Label>Content</Form.Label>
@@ -28,6 +50,7 @@ const CreatePostForm = ({ newPost, handleInputChange, handlePostSubmit }) => {
                     value={newPost.content}
                     onChange={handleInputChange}
                 />
+                {!validation.content && <div className="validation-message">Content can't be empty</div>}
             </Form.Group>
             <Form.Group controlId="postCategory">
                 <Form.Label>Category</Form.Label>
@@ -50,6 +73,7 @@ const CreatePostForm = ({ newPost, handleInputChange, handlePostSubmit }) => {
                         ))}
                     </Dropdown.Menu>
                 </Dropdown>
+                {!validation.category && <div className="validation-message">Category can't be empty</div>}
             </Form.Group>
             <Button variant={isDarkMode ? 'create-post-dark' : 'create-post-light'} type="submit">
                 Create Post
