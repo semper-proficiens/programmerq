cc64731187eb232ba46f5db4af45172bddd7ac2b
nubesFilius <eperezlarosa@outlook.com>
2024-02-19 21:17:26 -0500

    fix queued squares animation

--- react-app/src/components/QueuedSquares.js
+++ react-app/src/components/QueuedSquares.js
@@ -8,7 +8,7 @@ function QueuedSquares() {
     for (let i = 0; i < numSquares; i++) {
         squares.push(
             <div key={i} className="animated-object" style={{ animationDelay: `${i * 0.2}s`, top: `${(i + 1) * 100}px` }}>
-                {i === 0 ? 'Programmer...' : `Queue${i}`}
+                {i === 0 ? 'Programmer...' : `Queue${i -1}`}
             </div>
         );
     }
