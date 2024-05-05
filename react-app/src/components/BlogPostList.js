3711fc5e0c70a87ccdd2a097109ba4cfd5253385
nubesFilius <eperezlarosa@outlook.com>
2024-05-05 16:51:07 -0400

    improve on the blogcategorypostlist component

--- react-app/src/components/BlogPostList.js
+++ react-app/src/components/BlogPostList.js
@@ -1,10 +1,10 @@
 import React, {useContext} from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { DarkModeContext } from '../contexts/DarkModeContext';
-import '../styles/blogpostcategorywidget.css';
+import '../styles/blogcategorypostslist.css';
 
 const blogPosts = {
-    'Hardware': ['ServerRack - Why you should build one', 'Post 2', 'Post 3'],
+    'Hardware': ['ServerRack', 'Post 2', 'Post 3'],
     'Security': ['Post 4', 'Post 5', 'Post 6'],
     'App Performance': ['Post 7', 'Post 8', 'Post 9'],
     'CI/CD': ['Post 10', 'Post 11', 'Post 12'],
@@ -19,12 +19,14 @@ function BlogPostList() {
     const posts = blogPosts[category];
 
     return (
-        <div className={isDarkMode ? 'post dark-mode' : 'post'}>
-            <h2>{category} Posts</h2>
+        <div className={isDarkMode ? 'post-list dark-mode' : 'post-list'}>
+            <h2 className="blog-header">{category} Posts</h2>
             <ul>
                 {posts.map((post, index) => (
-                    <li key={index}>
-                        <Link to={`/post/${post.replace(/\s/g, '-')}`}>{post}</Link>
+                    <li key={index} className="post-item">
+                        <Link to={`/post/${post.replace(/\s/g, '-')}`} className="blog-post-link">
+                            {post}
+                        </Link>
                     </li>
                 ))}
             </ul>
