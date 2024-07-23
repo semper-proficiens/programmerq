74d8f721b293b2a20013a5473ac051807573b359
nubesFilius <eperezlarosa@outlook.com>
2024-07-23 17:49:10 -0400

    update_AutomateFrontendDeployment

--- react-app/src/components/posts/CICD/AutomateFrontendDeployment.js
+++ react-app/src/components/posts/CICD/AutomateFrontendDeployment.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { DarkModeContext } from '../../../contexts/DarkModeContext';
 import '../../../styles/post.css';
-import SecureLock from '../../../assets/images/secure-on-chalkboard-with-locks.jpg';
+import CICD_ProgrammerQ_Frontend from '../../../assets/images/CICD_ProgrammerQ_Frontend.drawio.png';
 import CollapsibleSection from '../../CollapsibleSection';
 import BlogPostIndentedParagraph from "../../BlogPostIndentedParagraph";
 
@@ -12,7 +12,7 @@ function AutomateFrontendDeployment() {
 
         <article className={isDarkMode ? 'post dark-mode' : 'post'}>
 
-            <img className="post-image" src={SecureLock} alt="SecureLock" />
+            <img className="post-image" src={CICD_ProgrammerQ_Frontend} alt="CICD_Diagram" />
 
             <CollapsibleSection title="Why spend time automating this? (~1min)">
                 <BlogPostIndentedParagraph>
@@ -29,15 +29,19 @@ function AutomateFrontendDeployment() {
                 </BlogPostIndentedParagraph>
             </CollapsibleSection>
 
-            <CollapsibleSection title="Workflow explained (~min)">
+            <CollapsibleSection title="Workflow explained (~5 min)">
                 <BlogPostIndentedParagraph>
                     So, how do I automate my deployments from a commit to github main to a live change in my webpage?
                     My CI/CD workflow is just a baby at the moment (I'll update it as I introduced more components and features).
-                    Let's break it down in pieces.
+                    You can see what it looks like above (diagram at the top of the page).
                 </BlogPostIndentedParagraph>
 
-                <BlogPostIndentedParagraph>
+            </CollapsibleSection>
 
+            <CollapsibleSection title="Pointer to each setup (~1min)">
+                <BlogPostIndentedParagraph>
+                    Hopefully, I got you hooked, and you want to do the same.
+                    Well, you can see how I deployed each component here.
                 </BlogPostIndentedParagraph>
 
             </CollapsibleSection>
